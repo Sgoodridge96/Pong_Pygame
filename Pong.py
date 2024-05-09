5e0deb73908ad0954a124e399f64e3f1946746db
sgoodridge96 <sgoodridge96@gmail.com>
2024-05-09 19:00:51 -0400

    Temp timer fix, fixed collisions on walls, cleaned up code

--- Pong.py
+++ Pong.py
@@ -39,18 +39,20 @@ class PongBall:
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
 
-    def checkCollisions(self, screen_height, screen_width):
-        if self.y < 0 or self.y > screen_height:
+    def checkCollision(self, screen_height, screen_width, radius):
+        #Check wall collisions
+        if self.y < 0 + radius or self.y > screen_height - radius:
             self.speed[1] *= -1
-        if self.x < 0 or self.x > screen_width:
+        if self.x < 0 + radius or self.x > screen_width - radius:
             self.speed[0] *= -1
-
+    
+    
 
 def ballDirection():
     #Randomly pick a ball direction and speed at the start of the game.  
     random.seed()
-    ball_direction_x = random.randint(-3,3)
-    ball_direction_y = random.randint(-3,3)
+    ball_direction_x = random.randint(-2,2)
+    ball_direction_y = random.randint(-2,2)
 
     #Make sure the ball can't go straight up and down or left to right.  
     if ball_direction_x == 0:
@@ -58,77 +60,73 @@ def ballDirection():
     if ball_direction_y == 0:
         ball_direction_y += 1
     
-    print(ball_direction_y)
     return ball_direction_x, ball_direction_y
 
-
-
 def main():
-
-    ## Game Definitions ##
-    ####################################################################################################
-
-    # Set up the screen dimensions
+    
+    # Define screen dimensions
     screen_width = 800
     screen_height = 600
 
-    # Define the width and height of the rectangle
+    # Define the width and height of player rectangle
     rect_width = 25
     rect_height = 80
 
     #Define PongBall
     pongBall_radius = 10
     pongBall_speed = ballDirection()
-
-    #pygame uses milliseconds, set 6000 milliseconds to convert to 60 seconds later
-    clock = pygame.time.Clock()
-    clock_timer = 6000
+    
+    #Define Starting timer
+    starting_timer = 120
 
     # Define colors
     background = (0, 0, 0)
     actorColor = (230, 255, 255)
-
-    ####################################################################################################
-
+    
     #Setup and start Pong game
-
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Pong")
 
-    # Spawn Rectangle (position, size, color, speed)
+    # Spawn Objects
     playerRect = PlayerRectangle((screen_width - rect_width) // 16, (screen_height - rect_height) // 2, rect_width, rect_height, actorColor, 5)
     pongBall = PongBall((screen_width - pongBall_radius) // 2, (screen_height - pongBall_radius) // 2, pongBall_radius, actorColor, pongBall_speed)
 
-    running = True
 
+    running = True
     while running:
-        
-        clock_timer -= 1
-        if clock_timer <= 0:
-            running = False
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        #Render clock and turn off game when it hits zero
-        print(screen_width, screen_height)
+        #Logic to countdown timer ("// 1000" for milliseconds to seconds)
+        clock_timer = pygame.time.get_ticks()
+        current_timer = starting_timer - clock_timer // 1000
+        
+        #Render clock and exit game when it hits zero
         timer_text = pygame.font.SysFont(None, 36)
-        render_timer = timer_text.render("Time: " + str(clock_timer // 1000), True, (230, 255, 255))
+        render_timer = timer_text.render("Time: " + str(current_timer) + " seconds", True, (230, 255, 255))
 
+        #Player movement
         keys = pygame.key.get_pressed()
         playerRect.move(keys, screen_height)
 
+        #Fill screen and draw objects
         screen.fill(background)
         playerRect.draw(screen)
-        pongBall.checkCollisions(screen_height, screen_width)
+        pongBall.checkCollision(screen_height, screen_width, pongBall_radius)
         pongBall.move()
         pongBall.draw(screen)
 
-        
+        #render timer to screen
         screen.blit(render_timer, (350, 25))
+
         pygame.display.flip()
-        clock.tick(60)
+        pygame.time.Clock().tick(60)
+        
+        #If timer hits 0, close game (Fix later with game over screen)
+        if current_timer <= 0:
+            running = False
 
     pygame.quit()
 main()
